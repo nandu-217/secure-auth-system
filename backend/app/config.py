213cfddf8a78a5578c1ec90643fd991c05db6301
nandini <garanandini067@gmail.com>
2026-05-26 11:35:07 +0530

    Fix PostgreSQL connection pool

--- backend/app/config.py
+++ backend/app/config.py
@@ -31,54 +31,28 @@ class Config:
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # MAIL SETTINGS
-    MAIL_SERVER = os.getenv(
-        "MAIL_SERVER"
-    )
+    
+
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
 
     MAIL_PORT = int(
-        os.getenv(
-            "MAIL_PORT",
-            587
-        )
-    )
-
-    MAIL_USE_TLS = (
-        os.getenv(
-            "MAIL_USE_TLS",
-            "True"
-        ) == "True"
-    )
-
-    MAIL_USERNAME = os.getenv(
-        "MAIL_USERNAME"
-    )
-
-    MAIL_PASSWORD = os.getenv(
-        "MAIL_PASSWORD"
-    )
-    # MAIL SETTINGS
-
-MAIL_SERVER = os.getenv("MAIL_SERVER")
-
-MAIL_PORT = int(
     os.getenv("MAIL_PORT", 587)
 )
 
-MAIL_USE_TLS = True
+    MAIL_USE_TLS = True
 
-MAIL_USE_SSL = False
+    MAIL_USE_SSL = False
 
-MAIL_USERNAME = os.getenv(
+    MAIL_USERNAME = os.getenv(
     "MAIL_USERNAME"
-)
+   )
 
-MAIL_PASSWORD = os.getenv(
+    MAIL_PASSWORD = os.getenv(
     "MAIL_PASSWORD"
-)
+   )
 
-MAIL_DEFAULT_SENDER = os.getenv(
+    MAIL_DEFAULT_SENDER = os.getenv(
     "MAIL_USERNAME"
-)
+   )
 print("DATABASE_URL =", os.getenv("DATABASE_URL"))
 #print("SQLALCHEMY_DATABASE_URI =", app.config["SQLALCHEMY_DATABASE_URI"])
